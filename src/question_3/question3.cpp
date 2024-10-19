c9b50fa6b89a3935629d8080fbe711e92112e8db
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-10-19 18:45:48 -0500

    Update question3.cpp

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,19 +1,7 @@
-#include "question3.h"
+// conversion.cpp
+#include "conversion.h"
 
-bool test_config()
-
-    int get_earned_points(int sold) {
-    if (sold >= 1 && sold <= 5) {
-        return sold * 1;      // 1 point per widget sold (1-5)
-    } else if (sold >= 6 && sold <= 10) {
-        return sold * 5;      // 5 points per widget sold (6-10)
-    } else if (sold >= 11 && sold <= 15) {
-        return sold * 10;     // 10 points per widget sold (11-15)
-    } else if (sold >= 16) {
-        return sold * 15;     // 15 points per widget sold (16 or more)
-    } else {
-        return 0;             // 0 points for 0 widgets sold
-    }
-
-    return true;
+double get_fahrenheit(double c) {
+    return (9.0 / 5.0) * c + 32;
 }
+
