0fc8ec8ac432ae48a1b82fe6ddb4eba1cb40e407
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-10-19 19:05:14 -0500

    Update question_tests_2.cpp

--- test/question_test_2/question_tests_2.cpp
+++ test/question_test_2/question_tests_2.cpp
@@ -1,12 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "question2.h"
+#include "points.h"  // Make sure to include the correct header file
 
 TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+    REQUIRE(true == true);
 }
 
-TEST_CASE("test")
-{
-	REQUIRE(test_config() == true);
+TEST_CASE("Points earned based on widgets sold") {
+    REQUIRE(get_earned_points(3) == 3);    // 1 point each for 3 widgets
+    REQUIRE(get_earned_points(9) == 45);   // 5 points each for 9 widgets
+    REQUIRE(get_earned_points(11) == 110);  // 10 points each for 11 widgets
+    REQUIRE(get_earned_points(20) == 300);  // 15 points each for 20 widgets
 }
+
