885dec445feb8c71dc0e618f979e714d17a93cff
ProgrammingNoobi <angelo.puente@g.austincc.edu>
2024-10-19 20:09:22 -0500

    Update question4.h

--- src/question_4/question4.h
+++ src/question_4/question4.h
@@ -1,2 +1,10 @@
-//
 bool test_config();
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+#include <string>
+
+// Function prototype for get_factorial_sequence
+std::string get_factorial_sequence(int n);
+
+#endif // FACTORIAL_H
